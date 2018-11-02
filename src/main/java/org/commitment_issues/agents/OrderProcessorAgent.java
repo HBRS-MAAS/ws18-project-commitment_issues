dc2525c8fbd9242f920d9dbf5c9691dd303e4cb1
Unknown <ahmedfaisal_95@hotmail.co.uk>
2018-11-02 19:41:17 +0100

    Add order server behaviour for order processor agents minor change

--- src/main/java/org/commitment_issues/agents/OrderProcessorAgent.java
+++ src/main/java/org/commitment_issues/agents/OrderProcessorAgent.java
@@ -13,40 +13,66 @@ import jade.lang.acl.ACLMessage;
 @SuppressWarnings("serial")
 public class OrderProcessorAgent extends Agent {
 	protected void setup() {
-	// Printout a welcome message
+		// Printout a welcome message
 		System.out.println("Hello! OrderProcessor-agent "+getAID().getName()+" is ready.");
 
-        try {
- 			Thread.sleep(3000);
- 		} catch (InterruptedException e) {
- 			//e.printStackTrace();
- 		}
-		addBehaviour(new shutdown());
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			//e.printStackTrace();
+		}
+		
+		addBehaviour(new OrderServer());
+		
+//		addBehaviour(new shutdown());
 
 	}
-	
+
 	protected void takeDown() {
 		System.out.println(getAID().getLocalName() + ": Terminating.");
 	}
-	
-	// Taken from http://www.rickyvanrijn.nl/2017/08/29/how-to-shutdown-jade-agent-platform-programmatically/
-		private class shutdown extends OneShotBehaviour{
-			public void action() {
-				ACLMessage shutdownMessage = new ACLMessage(ACLMessage.REQUEST);
-				Codec codec = new SLCodec();
-				myAgent.getContentManager().registerLanguage(codec);
-				myAgent.getContentManager().registerOntology(JADEManagementOntology.getInstance());
-				shutdownMessage.addReceiver(myAgent.getAMS());
-				shutdownMessage.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
-				shutdownMessage.setOntology(JADEManagementOntology.getInstance().getName());
-				try {
-				    myAgent.getContentManager().fillContent(shutdownMessage,new Action(myAgent.getAID(), new ShutdownPlatform()));
-				    myAgent.send(shutdownMessage);
-				}
-				catch (Exception e) {
-				    //LOGGER.error(e);
-				}
 
+	// Taken from http://www.rickyvanrijn.nl/2017/08/29/how-to-shutdown-jade-agent-platform-programmatically/
+	private class shutdown extends OneShotBehaviour{
+		public void action() {
+			ACLMessage shutdownMessage = new ACLMessage(ACLMessage.REQUEST);
+			Codec codec = new SLCodec();
+			myAgent.getContentManager().registerLanguage(codec);
+			myAgent.getContentManager().registerOntology(JADEManagementOntology.getInstance());
+			shutdownMessage.addReceiver(myAgent.getAMS());
+			shutdownMessage.setLanguage(FIPANames.ContentLanguage.FIPA_SL);
+			shutdownMessage.setOntology(JADEManagementOntology.getInstance().getName());
+			try {
+				myAgent.getContentManager().fillContent(shutdownMessage,new Action(myAgent.getAID(), new ShutdownPlatform()));
+				myAgent.send(shutdownMessage);
+			}
+			catch (Exception e) {
+				//LOGGER.error(e);
+			}
+
+		}
+	}
+
+	private class OrderServer extends CyclicBehaviour {
+		public void action() {
+			ACLMessage msg = myAgent.receive();
+			
+			if (msg != null) {
+				String orderDetails = msg.getContent();
+				ACLMessage reply = msg.createReply();
+				
+				reply.setPerformative(ACLMessage.INFORM);
+				reply.setContent("order-received.");
+				
+				System.out.println("["+getAID().getLocalName()+"]: Order received: "+orderDetails);
+
+				myAgent.send(reply);
+			}
+			
+			else {
+				block();
 			}
 		}
+	} 
+
 }

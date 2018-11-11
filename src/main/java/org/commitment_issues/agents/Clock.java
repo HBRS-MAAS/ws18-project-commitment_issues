5fcd59cbb201d6bf75b3809aa5b1b82398d01722
AbanoubAbdelmalak <abanoub_157@yahoo.com>
2018-11-11 22:49:02 +0100

    Created Clock agent to increase one hour each second

---
+++ src/main/java/org/commitment_issues/agents/Clock.java
@@ -0,0 +1,41 @@
+package org.commitment_issues.agents;
+
+import jade.domain.AMSService;
+import jade.domain.FIPAAgentManagement.*;
+import jade.lang.acl.ACLMessage;
+import jade.core.Agent;
+import jade.core.behaviours.TickerBehaviour;
+
+import java.util.ArrayList;
+
+import jade.core.AID;
+
+
+
+public class Clock extends Agent {
+  static AMSAgentDescription [] agents = null;
+  public void setup() {
+    System.out.println("Hello! Clock-agent "+getAID().getName()+" is ready each second is one hour.");
+    
+    try {
+      SearchConstraints c = new SearchConstraints();
+      c.setMaxResults ( new Long(-1) );
+      agents = AMSService.search( this, new AMSAgentDescription (), c );
+  }
+  catch (Exception e) {
+    
+  }
+    addBehaviour(new TickerBehaviour(this, 1000) {
+      protected void onTick() {
+        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+        
+        for (int i=0; i<agents.length;i++){
+          msg.addReceiver(Clock.agents[i].getName());
+     }
+        msg.setContent("Hour_has_passed");
+        myAgent.send(msg);
+      }
+    });
+
+  }
+}

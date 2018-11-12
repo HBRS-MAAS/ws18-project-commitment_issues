de612eb6c07a9875af725f16ebbce2ee4f55fff8
Abanoub Abdelmalak <abanoub_157@yahoo.com.com>
2018-11-12 18:26:33 +0100

    The clock now sends the current time as a String

--- src/main/java/org/commitment_issues/agents/Clock.java
+++ src/main/java/org/commitment_issues/agents/Clock.java
@@ -10,9 +10,15 @@ import jade.core.behaviours.TickerBehaviour;
 
 public class Clock extends Agent {
   private static AMSAgentDescription [] agents = null;
+  private int hours = 0;
+  private int days = 0;
   public void setup() {
     System.out.println("Hello! Clock-agent "+getAID().getName()+" is ready each second is one hour.");
-    
+//    try {
+//      Thread.sleep(3000);
+//    } catch (InterruptedException e) {
+//      //e.printStackTrace();
+//    }
     try {
       SearchConstraints c = new SearchConstraints();
       c.setMaxResults ( new Long(-1) );
@@ -24,14 +30,33 @@ public class Clock extends Agent {
     addBehaviour(new TickerBehaviour(this, 1000) {
       protected void onTick() {
         ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-        
+        hours++;
+        if (hours == 24){
+          days++;
+          hours = 0;
+        }
         for (int i=0; i<agents.length;i++){
           msg.addReceiver(Clock.agents[i].getName());
      }
-        msg.setContent("Hour_has_passed");
+        msg.setContent(Clock.showTime(hours, days));
+        msg.setConversationId("Time-Update");
         myAgent.send(msg);
       }
     });
 
+  }
+  protected static String showTime(int h, int d) {
+    String curTime = Integer.toString(h)+"";
+    if (curTime.length() == 1) {
+      curTime = "0" + curTime;
+    }
+    curTime = "." + curTime;
+    String hh = Integer.toString(d);
+    while (hh.length() < 3) {
+      hh = "0" + hh;
+    }
+    curTime = hh + curTime;
+    
+  return curTime;
   }
 }

4d043f5b90efee5b924caf6dd93db1e0164301eb
Unknown <ahmedfaisal_95@hotmail.co.uk>
2018-11-17 17:38:52 +0100

    Add references to source code for the implemented algorithm

--- src/main/java/org/commitment_issues/delivery_agents/Edge.java
+++ src/main/java/org/commitment_issues/delivery_agents/Edge.java
@@ -1,17 +1,18 @@
 package org.commitment_issues.delivery_agents;
 
+/*
+ * This class implements the an edge of a directed graph to be used in 
+ * the Dijkstra algorithm.
+ * 
+ * Courtesy of Lars Vogel (c) 2009, 2016 vogella GmbH. Version 1.2, 29.09.2016
+ * Source: http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
+ */
+
 public class Edge  {
     private final String id;
     private final Vertex source;
     private final Vertex destination;
     private final float weight;
-
-//    public Edge(String id, Vertex source, Vertex destination, int weight) {
-//        this.id = id;
-//        this.source = source;
-//        this.destination = destination;
-//        this.weight = weight;
-//    }
     
     public Edge(String id, Vertex source, Vertex destination, float weight) {
         this.id = id;
@@ -30,9 +31,6 @@ public class Edge  {
     public Vertex getSource() {
         return source;
     }
-//    public int getWeight() {
-//        return weight;
-//    }
     
     public float getWeight() {
         return weight;

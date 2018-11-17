65e28c5411282db9fa365749e50cf38ce5fe88ce
Abanoub Abdelmalak <abanoub_157@yahoo.com.com>
2018-11-18 00:20:58 +0100

    Added the streetNetwork parser behaviour to show the edges

--- src/main/java/org/commitment_issues/delivery_agents/GraphVisualizationAgent.java
+++ src/main/java/org/commitment_issues/delivery_agents/GraphVisualizationAgent.java
@@ -27,7 +27,6 @@ public class GraphVisualizationAgent extends Agent {
   Graph graph = new Graph();
   Model model = graph.getModel();
 
-  
   public void setup() {
     System.out.println("Hello! Visualization-agent "+getAID().getName()+" is ready each second is one hour.");
     yellowPageRegister();
@@ -75,33 +74,59 @@ public class GraphVisualizationAgent extends Agent {
     @Override
     public void action() {
       ACLMessage recieve = myAgent.receive();
+      
       if (recieve != null && recieve.getConversationId().equals("initial-state")) {
+        
         JSONObject wholeMsg = new JSONObject(recieve);
+        
         JSONArray nodes = wholeMsg.getJSONArray("Nodes");
         JSONArray edges = wholeMsg.getJSONArray("Edges");
+        
         for (int i = 0; i < nodes.length(); i++) {
+          
           JSONObject node = nodes.getJSONObject(i);
+          
           int type = node.getInt("Type");
           // i for bakeries and 0 for customers
           JSONObject location = node.getJSONObject("Location");
           float nodeX = location.getFloat("X")*(float)10.0;
           float nodeY = location.getFloat("Y")*(float)10.0;
+          
           String nodeID = node.getString("NodeID");
+          
           if (type == 0) {
             shape = CellType.RECTANGLE;
           }
           else{
             shape = CellType.TRIANGLE;
           }
+          
           graph.beginUpdate();
           model.addCell(nodeID, shape);
           graph.endUpdate();
+          
           model.getAllCells().get(i).relocate(nodeX, nodeY);
         }
+        
+        for (int k = 0;k < edges.length(); k++) {
+          
+          JSONObject edge = edges.getJSONObject(k);
+          
+          String startNode = edge.getString("startNodeID");
+          String endNode = edge.getString("endNodeID");
+          
+          graph.beginUpdate();
+          model.addEdge(startNode, endNode);
+          graph.endUpdate();
+        
+        }
+        
       m.setGraph(graph);
       }
       else {
+        
         block();
+      
       }
 
     }

0f4b26ac54f765d99928577ad96ae61469526158
Unknown <ahmedfaisal_95@hotmail.co.uk>
2019-01-17 16:20:57 +0100

    Fix entry agent order processing behavior conversation ID

--- src/main/java/org/commitment_issues/packaging_agents/EntryAgent.java
+++ src/main/java/org/commitment_issues/packaging_agents/EntryAgent.java
@@ -288,6 +288,7 @@ public class EntryAgent extends BaseAgent {
 		public void action() {
 			AID[] allAgents = findAllAgents();
 			ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+			msg.setConversationId("order");
 			msg.setContent(orderObj.toString());
 			for (AID agent : allAgents) {
 				msg.addReceiver(agent);
